8e0b07a9458d17e2eb70ee79c30403aed03dddf9
sosualfred <sosualfred@gmail.com>
2021-02-18 17:01:52 +0000

    refactored theme configurations

--- lib/utils/themeconfigurations.dart
+++ lib/utils/themeconfigurations.dart
@@ -5,23 +5,31 @@ ThemeData themeConfigurations() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
     fontFamily: 'Muli',
-    appBarTheme: AppBarTheme(
-      color: Colors.white,
-      elevation: 0,
-      centerTitle: true,
-      brightness: Brightness.light,
-      iconTheme: IconThemeData(color: Colors.black),
-      textTheme: TextTheme(
-        headline6: TextStyle(
-          color: Color(0XFF8B8B8B),
-          fontSize: 18,
-        ),
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+TextTheme textTheme() {
+  return TextTheme(
+    bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    centerTitle: true,
+    brightness: Brightness.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Color(0XFF8B8B8B),
+        fontSize: 18,
       ),
     ),
-    textTheme: TextTheme(
-      bodyText1: TextStyle(color: kTextColor),
-      bodyText2: TextStyle(color: kTextColor),
-    ),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
