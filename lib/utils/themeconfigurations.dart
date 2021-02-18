1fa0dafcd2f1c260fd09f42de37228b3036a4801
sosualfred <sosualfred@gmail.com>
2021-02-18 19:35:23 +0000

    change primary color, add input decoration theme

--- lib/utils/themeconfigurations.dart
+++ lib/utils/themeconfigurations.dart
@@ -3,11 +3,27 @@ import 'package:mobileapp/utils/contants.dart';
 
 ThemeData themeConfigurations() {
   return ThemeData(
+    primaryColor: Colors.green,
     scaffoldBackgroundColor: Colors.white,
     fontFamily: 'Muli',
     appBarTheme: appBarTheme(),
     textTheme: textTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
     visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: BorderSide(color: kTextColor),
+    gapPadding: 10,
+  );
+  return InputDecorationTheme(
+    contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
   );
 }
 
