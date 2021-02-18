2e7db184757bb3bdf471919870a56291f255308b
sosualfred <sosualfred@gmail.com>
2021-02-18 19:37:09 +0000

    change the primary color to green

--- lib/utils/contants.dart
+++ lib/utils/contants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFFFF7643);
+// const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryColor = Color(0xFF007643);
 const kPrimaryLightColor = Color(0xFFFFECDF);
 const kPrimaryGradientColor = LinearGradient(
   begin: Alignment.topLeft,
