cb033ca6883433baf703f01f86ded8ed2cdcb724
kobbydiscount <32777408+kobbydiscount@users.noreply.github.com>
2021-03-02 13:24:03 +0000

    show-home-content

--- lib/screens/home/component/content.dart
+++ lib/screens/home/component/content.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/components/content_data.dart';
+import 'package:mobileapp/model/content_model.dart';
 
 class BodyContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ContentData(
-          image: "assets/images/home_image.jpg",
-          name: "Florence Hanson",
-          title: "Aid To Pursue BEcon",
-          message: "I started University last year but"
-              " due to unfortunate reasons...",
-          amount: "1,400",
-          percentage: "89 %",
-        ),
-      ],
+    return ListView.builder(
+      itemCount: contentmodel.length,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          children: [
+            ContentData(
+              image: "assets/images/home_image.jpg",
+              name: "Florence Hanson",
+              title: "Aid To Pursue BEcon",
+              message: "I started University last year but"
+                  " due to unfortunate reasons...",
+              amount: "1,400",
+              percentage: "89 %",
+            ),
+          ]
+        );
+      },
     );
   }
 }
-
-
