616c12f5bbff1fa29b96ec3322a18902dc6bce1a
sosualfred <sosualfred@gmail.com>
2021-03-09 21:05:22 +0000

    move application card to applications folder

--- lib/screens/home/components/application_list.dart
+++ lib/screens/home/components/application_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
-import 'package:mobileapp/components/application_card.dart';
+import 'package:mobileapp/screens/home/components/application_card.dart';
 import 'package:mobileapp/controllers/application_controller.dart';
 
 class ApplicationList extends StatelessWidget {
