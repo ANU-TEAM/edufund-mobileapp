8a5af6661ff4d999c30983c92eeff43214c46e4c
sosualfred <sosualfred@gmail.com>
2021-03-11 14:57:09 +0000

    add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+User userFromJson(String str) => User.fromJson(json.decode(str));
+
+String userToJson(User data) => json.encode(data.toJson());
+
+class User {
+  User({
+    this.user,
+    this.token,
+  });
+
+  UserClass user;
+  String token;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        user: UserClass.fromJson(json["user"]),
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "user": user.toJson(),
+        "token": token,
+      };
+}
+
+class UserClass {
+  UserClass({
+    this.name,
+    this.email,
+  });
+
+  String name;
+  String email;
+
+  factory UserClass.fromJson(Map<String, dynamic> json) => UserClass(
+        name: json["name"],
+        email: json["email"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "email": email,
+      };
+}
