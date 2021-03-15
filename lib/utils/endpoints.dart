2b3c5b6b4b0a0d20fc8b13af3f023696bc67e82e
sosualfred <sosualfred@gmail.com>
2021-03-15 20:01:54 +0000

    added forgot password endpoint

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -2,10 +2,10 @@ class AppUrl {
   static const String liveBaseURL = "https://edufundanu.herokuapp.com/api";
   static const String localBaseURL = "http://10.11.10.47:8001/api";
 
-  static const String baseURL = localBaseURL;
+  static const String baseURL = liveBaseURL;
 
   static const String login = baseURL + "/login";
   static const String register = baseURL + "/register";
-  // static const String forgotPassword = baseURL + "/forgot-password";
+  static const String forgotPassword = baseURL + "/forgot-password";
   static const String applications = baseURL + "/applications";
 }
