9c5849b0a04f9e600555c9ef39353e264ceea39c
kobbydiscount <32777408+kobbydiscount@users.noreply.github.com>
2021-03-23 19:36:10 +0000

    fixes #75

--- lib/screens/info/info.dart
+++ lib/screens/info/info.dart
@@ -2,34 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobileapp/components/default_button.dart';
 import 'package:mobileapp/screens/feedback/feedback.dart';
+import 'package:mobileapp/utils/contants.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Center(
-              child: Text(
-                "EDUFUND",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+        bottomNavigationBar: BottomAppBar(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            child: DefaultButton(
+              text: ("Send Feedback"),
+              press: () {
+                Get.to(() => FeedbackScreen());
+              },
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                child: Image.asset(
+                  "assets/images/info.png",
+                  fit: BoxFit.contain,
+                  height: 280,
+                  width: MediaQuery.of(context).size.width,
                 ),
               ),
-            ),
-            SizedBox(
-              width: 200,
-              child: DefaultButton(
-                text: ("Send Feedback"),
-                press: () {
-                  Get.to(() => FeedbackScreen());
-                },
+              SizedBox(
+                height: 15,
               ),
-            ),
-          ],
+              Center(
+                child: Text(
+                  "EDUFUND is a 2021 Google Solution Challenge Platform "
+                  "which provides the avenue for those whose ambition has "
+                  "been halted becuase of financial reasons to realise their dream. "
+                  "The platform solicit for financial support from donors and "
+                  "allot them to deserving individuals.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Developed By: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    " Sosu Alfred and Kennedy Owusu",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Contact Info: ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    " All Nations University DSC",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
