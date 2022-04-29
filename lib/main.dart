2d45a4f8f1994c0ce186dd4725f02310016751c5
sosualfred <sosualfred@gmail.com>
2022-04-29 20:32:08 +0000

    add firebase to flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'EDU FUND',
+      title: 'EduFund GH',
       theme: themeConfigurations(),
       initialRoute: initScreen == 0 || initScreen == null ? "first" : "/",
       routes: {
